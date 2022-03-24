8833c68131e1bba0ee65b35b423f684347e5c772
Holly Christopher <holly_christopher@hotmail.com>
2022-03-24 15:55:23 +0000

    add modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import Nav from "./Components/Navbar";
 import Card from "./Components/Card";
+import ModalCard from "./Modal/ModalCard"
+import ModalBasket from "./Modal/ModalBasket";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
 const App = () => {
+  const [images, setImages] = useState([""])
+  const fetchImage = async () => {
+      let response = await fetch("https://api.thecatapi.com/v1/images/search?mime_types=jpg&limit=20&api_key=2ab73ab1-f5ca-4a8a-bf86-25e64a0f6abf")
+      let data = await response.json()
+      setImages(data)
+  }
+  
+  useEffect(() => {
+      fetchImage()
+  }, [])
+
   return (
     <div>
       <Nav/>
       <h1>Cats4Life</h1>
-      <Card />
+      <ModalCard />
+      <ModalBasket />
+      <Card images={images}/>
+      
+
   </div> 
   )
 };
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,34 +1,26 @@
-import { useState } from "react";
-import { useEffect } from "react";
- import faker from "faker";
 
-const Card = () => {
-    const [images, setImages] = useState([""])
-    const fetchImage = async () => {
-        let response = await fetch("https://api.thecatapi.com/v1/images/search?limit=12")
-        let data = await response.json()
-        setImages(data)
-    }
+import faker from "faker";
+import ModalCard from "../Modal/ModalCard";
+    
+const Card = ({ images }) => {
 
-    
-    useEffect(() => {
-        fetchImage()
-    }, [])
-    
     return (
         <div className="App">
           {images.map((item, index) => {
             return (
-              <div>
-                <img key={index} src={item.url} alt="img" />
-                <p>{faker.name.firstName()}</p>
-                <p>{faker.animal.cat()}</p>
-                <p>{faker.commerce.price()}</p>
-                <button> Add to cart</button>
+              <div key={index} id= {item.id} >
+                <img src={item.url} alt="img" />
+                <div id="name">{faker.name.firstName()}</div>
+                <div id="cat"> {faker.animal.cat()}</div>
+                <div id="price"> £{faker.commerce.price()}</div>
+                <h2>{item.id}</h2> 
+                <p>pass add to cart</p>
+                {/* <button className="add-tocart" onClick={ModalCard}>Add to cart</button> */}
               </div>
             );
           })}
         </div>
       );
-    };
+    }
+
 export default Card;
