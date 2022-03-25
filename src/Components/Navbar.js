45b297085703f0cabd4f368ad04f1cd6d1d36ede
Holly Christopher <holly_christopher@hotmail.com>
2022-03-25 10:32:13 +0000

    font added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,12 @@
 
 import Basket from "./Basket";
+import "./Navbarstyle.css"
 
 const Nav = ({ basketItems, removeFromBasket, image }) => {
   return (
     <div className="nav">
       <h1>Cats4Lyf</h1>
+      <h2>Adopt a furry friend today!</h2>
       <div className="basket">
         <Basket
           basketItems={basketItems}
